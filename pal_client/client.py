ac6da28f580f55d834c60125576a23c9d295c977
Andreas.Abdi <andreasabdi@yahoo.ca>
2017-06-09 12:10:03 -0700

    [C3TP-290] Client library for pal-client

--- pal_client/client.py
+++ pal_client/client.py
@@ -3,14 +3,55 @@
 
 import requests
 
-TARGET_URL = '127.0.0.1:5000'
+TARGET_URL = 'http://localsite:5000'
 
-def build_request():
-    requests.post(
-        url=TARGET_URL,
-        body={
-            'username': 'hi',
-            'password': 'dude'
+
+def download_file(b, k, username, password, target):
+    url = url_join(TARGET_URL, b, k, 'presigned_url')
+    response = requests.post(
+        url=url,
+        data={
+            'username': username,
+            'password': password
         }
     )
-    
+    presigned_url = response.text
+    response = requests.get(presigned_url)
+    with open(target, "wb") as target_file:
+        target_file.write(response.content)
+
+    print("hi")
+    return response
+
+
+def upload_file(b, k, username, password, target):
+    url = url_join(TARGET_URL, b, k, 'presigned_post')
+    response = requests.post(
+        url=url,
+        data={
+            'username': username,
+            'password': password
+        }
+    )
+    files = {"file": open(target, 'rb')}
+    data = response.json()
+    return requests.post(
+        data["url"],
+        data=data["fields"],
+        files=files)
+
+
+def symlink(b, k, username, password, target):
+    url = url_join(TARGET_URL, b, k, 'symlink')
+    return requests.post(
+        url=url,
+        data={
+            'username': username,
+            'password': password,
+            'target': target
+        }
+    )
+
+
+def url_join(*args):
+    return "/".join(map(lambda x: str(x).rstrip('/'), args))
